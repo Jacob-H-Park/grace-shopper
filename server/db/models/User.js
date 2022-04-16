180a5f944b7f4cf49496ed67837b993d2c55c6e4
kaisheng deng <carmen.deng12@gmail.com>
2022-04-16 13:39:07 -0400

    set up enviroment variable for carmen

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -3,7 +3,7 @@ const db = require("../db");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const axios = require("axios");
-
+require('dotenv').config()
 const SALT_ROUNDS = 5;
 
 const User = db.define("user", {
