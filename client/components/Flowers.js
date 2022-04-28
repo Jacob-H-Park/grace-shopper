1042fc2b55132434bf1946d57cd48ec8bc4c0b02
sichallez <sich.scidonnees@gmail.com>
2022-04-28 01:48:36 -0400

    fixed the bug of cart page re-render after login

--- client/components/Flowers.js
+++ client/components/Flowers.js
@@ -1,45 +1,9 @@
 import React, { Component, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import styled from "styled-components";
 
 import { fetchProducts } from "../store/flowers";
 import { addToCart } from "../store/order";
-
-import { css } from "styled-components";
-
-export const mobile = (props) => {
-  return css`
-    @media only screen and (max-width: 380px) {
-      ${props}
-    }
-  `;
-};
-
-const Container = styled.div``;
-const FilterContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-`;
-
-const Filter = styled.div`
-  margin: 20px;
-  ${mobile({ width: "0px 20px", display: "flex", flexDirection: "column" })}
-`;
-
-const FilterText = styled.span`
-  font-size: 20px;
-  font-weight: 600;
-  margin-right: 20px;
-  ${mobile({ marginRight: "0px" })}
-`;
-
-const Select = styled.select`
-  padding: 10px;
-  margin-right: 20px;
-  ${mobile({ margin: "10px 0px" })}
-`;
-const Option = styled.option``;
 
 
 const Flowers = () => {
