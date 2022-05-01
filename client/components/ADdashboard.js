e4a71e73abf4df728433100d75514ae5ac89063c
sichallez <sich.scidonnees@gmail.com>
2022-05-01 00:00:52 -0400

    added a couple of singleFlower images; admin dashboard demo

--- client/components/ADdashboard.js
+++ client/components/ADdashboard.js
@@ -1,12 +1,49 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Topbar from "./admin/Topbar";
+import Sidebar from "./admin/Sidebar";
+import Home from "./admin/Home";
+import UserList from "./admin/UserList";
+import User from "./admin/User";
+import NewUser from "./admin/newUser";
+import "./ADdashboard.css";
 
 const ADdashboard = ()=>{
     return (
         <div>
-            <Link to="/admin_products">View Products</Link>
+            {/* <Link to="/admin_products">View Products</Link>
             <Link to="/admin_orders">View Orders</Link>
-            <Link to="/admin_promotion">View Promotion</Link>
+            <Link to="/admin_promotion">View Promotion</Link> */}
+            <Router>
+                {/* <Topbar /> */}
+                <div className="container">
+                    <Sidebar />
+                    <Switch>
+                        <Route exact path="/">
+                            <Home />
+                        </Route>
+                        <Route path="/users">
+                            <UserList />
+                        </Route>
+                        <Route path="/user/:userId">
+                            <User />
+                        </Route>
+                        <Route path="/newUser">
+                            <NewUser />
+                        </Route>
+                        <Route path="/products">
+                            <ProductList />
+                        </Route>
+                        <Route path="/product/:productId">
+                            <Product />
+                        </Route>
+                        <Route path="/newproduct">
+                            <NewProduct />
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
         </div>
 
     )
