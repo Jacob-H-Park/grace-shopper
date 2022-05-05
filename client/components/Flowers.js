901ef2b1e6dc34949e913d5083dae3c4de8d150f
Trevor Latimer <trevorplatimer@gmail.com>
2022-05-05 13:28:08 +0200

    pagination works on homepage

--- client/components/Flowers.js
+++ client/components/Flowers.js
@@ -1,21 +1,16 @@
-import { AddShoppingCart } from "@mui/icons-material";
 import CloseIcon from "@mui/icons-material/Close";
 import {
   Box,
-  Card,
-  CardContent,
-  CardMedia,
   Grid,
   IconButton,
-  Typography,
   Snackbar,
   Slide,
+  Pagination
 } from "@mui/material";
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-
-import { addToCart } from "../store/order";
+import FlowerCard from "./FlowerCard";
+import ProductPagination from "./HelperComponents/ProductPagination";
 
 const Flowers = () => {
   //Redux hooks
@@ -24,7 +19,10 @@ const Flowers = () => {
   const dispatch = useDispatch();
 
   //React Hooks
-  const [name, setName] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [productsPerPage, setPostsPerPage] = useState(12);
+
 
   //Snackbar state hook
   const [state, setState] = useState({
@@ -35,11 +33,6 @@ const Flowers = () => {
   const SlideTransition = (props) => {
     return <Slide {...props} direction="up" />;
   };
-
-  //Helper fucntions
-  function handleChange(ev) {
-    setName(ev.target.value);
-  }
 
   //for snackbar
   const handleClick = (Transition) => {
@@ -69,21 +62,18 @@ const Flowers = () => {
       </IconButton>
     </>
   );
+  
+  //Pagination
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = flowers.slice(indexOfFirstProduct, indexOfLastProduct);
+
+  const paginate = (evt, value) => {
+    setCurrentPage(value);
+  }
 
   return (
     <div>
-      {/* <div>
-        <label htmlFor="flower-category"></label>
-        <select name="name" id="flower-category" onChange={handleChange}>
-          <option value="rose">Roses</option>
-          <option value="tulip">Tulips</option>
-          <option value="orchid">Orchids</option>
-          <option value="signature_bouquets">Signature Bouquets</option>
-          <option value="sympathy">Sympathy</option>
-          <option value="preserved_rose">Preserved Roses</option>
-        </select>
-      </div> */}
-
       {/* When a category is selected, page renders flowers by the given type */}
       <Box>
         <Box
@@ -96,104 +86,14 @@ const Flowers = () => {
             style={{ width: "100%" }}
           />
         </Box>
-        {name ? (
           <Grid container spacing={3} sx={{ padding: "2rem" }}>
-            {flowers
-              .filter((flower) => flower.category === name)
-              .map((flower) => {
-                return (
-                  <Grid item xs={3}>
-                    <Card key={flower.id} sx={{ maxWidth: "400px" }}>
-                      <Link to={`/flowers/${flower.id}`}>
-                        <CardMedia component="img" image={flower.image_url} />
-                      </Link>
-                      <Box
-                        sx={{
-                          display: "flex",
-                          alignItems: "center",
-                          justifyContent: "space-between",
-                        }}
-                      >
-                        <Box sx={{ display: "flex", flexDirection: "column" }}>
-                          <CardContent sx={{ flex: "1 0 auto" }}>
-                            <Typography
-                              sx={{ fontSize: "15px" }}
-                              component="div"
-                            >
-                              {flower.name}
-                            </Typography>
-                            <Typography sx={{ fontSize: "12px" }}>
-                              ${flower.price}
-                            </Typography>
-                          </CardContent>
-                        </Box>
-                        <Box>
-                          <IconButton
-                            sx={{ marginRight: "1rem" }}
-                            onClick={() => {
-                              dispatch(addToCart(user.id, flower));
-                              handleClick(SlideTransition);
-                            }}
-                          >
-                            <AddShoppingCart
-                              sx={{ height: "30px", width: "30px" }}
-                              color="secondary"
-                            />
-                          </IconButton>
-                        </Box>
-                      </Box>
-                    </Card>
-                  </Grid>
-                );
-              })}
-          </Grid>
-        ) : (
-          <Grid container spacing={3} sx={{ padding: "2rem" }}>
-            {flowers.map((flower) => {
+            {currentProducts.map((flower) => {
               return (
-                <Grid key={flower.id} item xs={3}>
-                  <Card key={flower.id} sx={{ maxWidth: "400px" }}>
-                    <Link to={`/flowers/${flower.id}`}>
-                      <CardMedia component="img" image={flower.image_url} />
-                    </Link>
-                    <Box
-                      sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "space-between",
-                      }}
-                    >
-                      <Box sx={{ display: "flex", flexDirection: "column" }}>
-                        <CardContent sx={{ flex: "1 0 auto" }}>
-                          <Typography sx={{ fontSize: "15px" }} component="div">
-                            {flower.name}
-                          </Typography>
-                          <Typography sx={{ fontSize: "12px" }}>
-                            ${flower.price}
-                          </Typography>
-                        </CardContent>
-                      </Box>
-                      <Box>
-                        <IconButton
-                          sx={{ marginRight: "1rem" }}
-                          onClick={() => {
-                            dispatch(addToCart(user.id, flower));
-                            handleClick(SlideTransition);
-                          }}
-                        >
-                          <AddShoppingCart
-                            sx={{ height: "30px", width: "30px" }}
-                            color="secondary"
-                          />
-                        </IconButton>
-                      </Box>
-                    </Box>
-                  </Card>
-                </Grid>
+                <FlowerCard flower={flower} user={user} SlideTransition={SlideTransition} handleClick={handleClick}/>
               );
             })}
           </Grid>
-        )}
+          <ProductPagination productsPerPage={productsPerPage} totalProducts={flowers.length} paginate={paginate} />
       </Box>
       <Snackbar
         onClose={handleClose}
