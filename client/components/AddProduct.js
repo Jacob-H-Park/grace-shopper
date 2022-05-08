dec05800b697bb8db0aa2011681977e881a3954f
kaisheng deng <carmen.deng12@gmail.com>
2022-05-08 02:44:01 -0400

    add styling to addproduct

--- client/components/AddProduct.js
+++ client/components/AddProduct.js
@@ -1,18 +1,51 @@
 import React from "react";
 import { createProducts } from "../store/flowers";
 import { connect } from "react-redux";
-import axios from 'axios';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import TextField from '@mui/material/TextField';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import { Button, Grid, Stack } from "@mui/material";
+import { withStyles } from "@material-ui/core/styles";
+import UploadPics from "./UploadPics";
+import LocalFloristSharpIcon from '@mui/icons-material/LocalFloristSharp';
+import IconButton from '@mui/material/IconButton';
 
-async function postImage({image}) {
-    const formData = new FormData();
-    formData.append("image", image)
-    const result = await axios.post('/api/images', formData, { headers: {'Content-Type': 'multipart/form-data'}})
-    return result.data
-  }
-  
+const categories = [
+    "rose",
+    "tulip",
+    "orchid",
+    "signature_bouquets",
+    "sympathy",
+    "preserved_rose",
+  ];
+
+const styles = theme => ({
+    input: {
+      height: 60
+    },
+    button: {
+      width: 10
+    },
+    selectRoot: {
+      height: 40,
+      display: "table"
+      // display: "flex",
+      // justifyContent: "center",
+      // alignItems: "center",
+    },
+    description: {
+      height: 150
+    }
+  });
+
 class AddProduct extends React.Component{
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
         this.state = {
             name:'',
             price: '',
@@ -20,11 +53,10 @@ class AddProduct extends React.Component{
             category: '',
             description: '',
             image_url: '',
-            file:''
         }
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
-        this.fileSelected = this.fileSelected.bind(this);
+        this.setUrl = this.setUrl.bind(this);
     }
     handleChange(evt) {
         this.setState({
@@ -32,14 +64,9 @@ class AddProduct extends React.Component{
         });
     }
     async handleSubmit(ev) {
-        const { name, price, stock, category,description,file} = this.state;
+        const { name, price, stock, category,description,image_url} = this.state;
         ev.preventDefault();
-        const result = await postImage({image:file})
-        const _imageUrl = result.imagePath
-        this.setState({
-            image_url: _imageUrl
-        })
-        const {image_url} = this.state
+        console.log('submit!new flower', this.state)
         const newFlower = {
             name,
             price,
@@ -50,47 +77,111 @@ class AddProduct extends React.Component{
         }
         this.props.createProduct({...newFlower})
     }
-    fileSelected(ev) {
-        console.log(ev.target.files)
-        const file = ev.target.files[0]
+
+    setUrl(url){
         this.setState({
-            file: file
+            image_url:url
         })
     }
     render(){
         const { name, price, stock, category,description} = this.state;
-        const { handleSubmit, handleChange,fileSelected } = this;
+        const { handleSubmit, handleChange, setUrl } = this;
+        const {classes} = this.props
         return(
-            <div className="productList">
-                <form onSubmit={handleSubmit}>
-                    <label>Product Name:</label>
-                    <input name = 'name' onChange={handleChange} value = {name}></input>
-        
-                    <label>Product Price:</label>
-                    <input name = 'price' onChange={handleChange} value = {price}></input>
-        
-                    <label>Product Category:</label>
-                    <select name = 'category' onChange={handleChange} value = {category}>
-                        <option value="rose">Roses</option>
-                        <option value="tulip">Tulips</option>
-                        <option value="orchid">Orchids</option>
-                        <option value="signature_bouquets">Signature Bouquets</option>
-                        <option value="sympathy">Sympathy</option>
-                        <option value="preserved_rose">Preserved Roses</option>
-                    </select>
-        
-                    <label>Product Stock:</label>
-                    <input name = 'stock' onChange={handleChange} value = {stock}></input>
-
-                    <label>Description:</label>
-                    <input name = 'description' onChange={handleChange} value = {description}></input>
-
-                    {/* to be able to upload images*/}
-                    <label>Product images:</label>
-                    <input id='imageInput' type="file" accept ="image/*" onChange={fileSelected}></input>
-                    <button type="submit">Submit</button>
-                </form>
-            </div>
+            
+            <Box
+            sx={{
+                display: 'flex',
+                flexWrap: 'wrap',
+                '& > :not(style)': {
+                m: 1,
+                width: "70%",
+                height: "100%",
+                },
+                flexDirection:'column',
+                justifyContent :"center",
+                margin: 8
+            }}
+            >
+                
+                <Paper elevation = {12} >
+                    <div className="productList">
+                        <form onSubmit={handleSubmit}>
+                            <Stack direction="column" spacing ={2} margin={2}> 
+                                <IconButton color="primary"  component="span">
+                                    <LocalFloristSharpIcon />Create Product
+                                </IconButton>
+                                
+                                <TextField 
+                                    name="name" 
+                                    label="Product Name" 
+                                    variant="outlined" 
+                                    onChange={handleChange} 
+                                    value = {name} 
+                                    InputProps={{
+                                        className: classes.input
+                                    }}
+                                />
+                               
+                                <FormControl sx={{ m: 1}}>
+                                    <InputLabel id="demo-multiple-name-label">Category</InputLabel>
+                                        <Select
+                                            name='category'
+                                            value={category}
+                                            onChange={handleChange}
+                                            input={<OutlinedInput label="category" />}
+                                        // MenuProps={MenuProps}
+                                        >
+                                        {categories.map((category) => (
+                                            <MenuItem
+                                                key={category}
+                                                value={category}
+                                            // style={getStyles(name, personName, theme)}
+                                            >
+                                                {category}
+                                            </MenuItem>
+                                        ))}
+                                        </Select>
+                                </FormControl>
+                                <TextField 
+                                    name="price" 
+                                    label="Product Price" 
+                                    variant="outlined" 
+                                    onChange={handleChange} 
+                                    value = {price} 
+                                    InputProps={{
+                                        className: classes.input
+                                    }}
+                                />      
+                                <TextField 
+                                    name="stock" 
+                                    label="Product Stock" 
+                                    variant="outlined" 
+                                    onChange={handleChange} 
+                                    value = {stock} 
+                                    InputProps={{
+                                        className: classes.input
+                                    }}
+                                />          
+                                <TextField 
+                                name="description" 
+                                label="Product Description" 
+                                variant="outlined" 
+                                onChange={handleChange} 
+                                value = {description} 
+                                InputProps={{
+                                    className: classes.description
+                                }}
+                                multiline
+                                row = {6}
+                                />
+                                <UploadPics setUrl = {setUrl}/>
+                                <Button variant="contained" type="submit">Submit</Button>
+                            </Stack>
+                        </form>
+                    </div>
+                </ Paper >
+            </Box> 
         )
     }
 }
@@ -104,5 +195,5 @@ const mapDispatch = (dispatch, ownProps) => {
     };
 };
 
-
-export default connect(null,mapDispatch)(AddProduct);
+const styledApp = withStyles(styles)(AddProduct)
+export default connect(null,mapDispatch)(styledApp);
